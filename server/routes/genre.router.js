e83478f25b60553c19d48abed32961bd3fc45a71
Billy Salinas <wsalinas315@gmail.com>
2022-12-11 00:22:08 -0600

    genre.router get by film ID written and tested

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -13,4 +13,18 @@ router.get('/', (req, res) => {
   });
 });
 
+// GET genres by movie id
+router.get('/:id', (req, res) => {
+  let queryID = req.params.id;
+  let genreQuery = `SELECT "name" FROM "genres" 
+                    JOIN "movies_genres" ON "movies_genres"."genre_id" = "genres"."id"
+                    WHERE "movie_id" = $1;`;
+  pool.query(genreQuery, [queryID]).then((result) => {
+    res.send(result.rows);
+  }).catch((error) => {
+    console.log('Error getting genres for specified film:', error);
+    res.sendStatus(500);
+  });
+});
+
 module.exports = router;
