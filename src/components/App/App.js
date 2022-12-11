658dac03efe8f5c881b55da982d3d6d0a236890b
Billy Salinas <wsalinas315@gmail.com>
2022-12-11 02:23:12 -0600

    commented out code removed from header.jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <h1>The Movies Saga!</h1> */}
       <Router>
         {/* Home page */}
         <Route path="/" exact>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 import './Header.css';
-// import { useHistory } from "react-router-dom";
-// import { Button } from "@material-ui/core";
-// import { useDispatch } from "react-redux";
 
 export default function Header(){
 
-  // initialize history
-  // const history = useHistory();
-
-  // initialize dispatch
-  // const dispatch = useDispatch();
-
-  // function to return to user to the home page
-  // const goHome = () =>{
-  //   dispatch({type: 'SET_DETAIL_HOME'});
-  //   history.push('/');
-  // }
-
   return(
     <div className="page-header">
-      {/* conditional rendering for a back button when not currently on the home page */}
-      {/* {headerText != "MovieList" && <div><Button variant='contained' onClick={() => goHome()}>Back To List</Button></div>} */}
       <h1>The Movies Saga!</h1>
     </div>
   )
