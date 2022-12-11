2b61cf66e0c3fab81ed746ac6fa641a7d35e3f09
Billy Salinas <wsalinas315@gmail.com>
2022-12-11 03:32:02 -0600

    text input fields added to AddMovie component

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,8 +1,7 @@
-import { useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Button, FormControl, TextField } from "@material-ui/core";
 import { useDispatch } from "react-redux";
-import React from 'react';
+import React, { useState } from 'react';
 import './AddMovie.css';
 
 export default function AddMovie() {
@@ -13,24 +12,46 @@ export default function AddMovie() {
   // initialize history
   const history = useHistory();
 
-  // function to return to user to the home page
+    // local state
+    const [movieTitle, setMovieTitle] = useState('');
+    const [moviePoster, setMoviePoster] = useState('');
+    const [description, setDescription] = useState('');
+    
+    // function to return to user to the home page
   const goHome = () => {
     // dispatch({ type: 'SET_DETAIL_HOME' });
     history.push('/');
   }
 
 
+  const submitMovie = (event) => {
 
+  }
 
   return (
-    <div>
-      <div> {/* Form control starts here */}
+    <form className="form-body" onSubmit={(event) => submitMovie(event)}>
         <div className='buttons'>
           <Button variant='contained' onClick={() => goHome()}>Cancel</Button>
           <Button variant='contained' type='submit'>Submit</Button>
         </div>
-      </div>
-    </div>
+        <FormControl>
+          <TextField required
+                    value={movieTitle} 
+                    label="Movie Title" 
+                    onChange={(event) => setMovieTitle(event.target.value)}
+                    variant="filled"/>
+          <TextField required
+                    value={moviePoster} 
+                    label="Movie Poster URL" 
+                    onChange={(event) => setMoviePoster(event.target.value)}
+                    variant="filled"/>
+          <TextField required
+                    value={description} 
+                    label="Description" 
+                    onChange={(event) => setDescription(event.target.value)}
+                    variant="filled"/>
+        </FormControl>
+    </form>
 
 
 
